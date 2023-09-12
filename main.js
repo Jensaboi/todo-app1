57c3cc8643c24c025301a4ff81451f423491046f
Jensaboi <107625279+Jensaboi@users.noreply.github.com>
2023-09-12 09:54:40 +0200

    updated

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let weeklyCalendarNav = 0;
-const weekDays = ["sun", "mån", "tis", "ons", "tor", "fre", "lör"]
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 const weeklyCalendarEl = document.getElementById("weekly-calendar")
@@ -9,56 +8,79 @@ const monthYearDay = document.getElementById("month-day-year")
 function renderWeeklyCalendar(){
   const dateObj = new Date()
   
-  const date = dateObj.getDate()
-  const day = dateObj.getDay()
-  const year = dateObj.getFullYear()
-  const month = dateObj.getMonth()
+  const currentDate = dateObj.getDate()
+  const currentWeekDayName = dateObj.getDay()
+  const currentYear = dateObj.getFullYear()
+  const currentMonth = dateObj.getMonth()
   
   //gets first day in month
-  const firstDayInMonth = new Date(year,month,1)
+  const firstDayInMonth = new Date(currentYear,currentMonth,1)
   //console.log(firstDayInMonth)
   
   //Gets last day in current month
-  const daysInMonth = new Date(year, month + 1, 0).getDate()
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate()
+  
+  //gets monday date of current week
+  const mondayDate = currentDate - currentWeekDayName + 1
   
   //Gets current weekday name
-  const getWeekDayName = (days) =>{
-    let day = new Date(Date.UTC(year,month, days))
+  const getWeekDayName = (year,month,days) =>{
+    let day = new Date(Date.UTC(year,month,days))
     const weekDay = {weekday: 'short'}
     return new Intl.DateTimeFormat('cet', weekDay).format(day)
   }
-  for(let i = 1; i < date; i++){
-    const weeklyDayEl = document.createElement('div')
-    weeklyDayEl.addEventListener("click",() =>{
-      console.log("clicked", i )
+  
+  for(let i = 0; i < 7; i++){
+    if(mondayDate + i > daysInMonth){
+      const weeklyDayEl = document.createElement('div')
+      weeklyDayEl.addEventListener("click",() =>{
+      console.log("clicked", mondayDate + i )
     })
     weeklyDayEl.className = "day-container"
-    weeklyDayEl.innerHTML = `<a href="#${i}"></a><p>${getWeekDayName(i)}</p><p class="weekly-calendar-day">${i}</p>`
-    
-    document.querySelector('#weekly-calendar').appendChild(weeklyDayEl);
-  }
+    weeklyDayEl.innerHTML = 
+    `<p>${getWeekDayName(i,i,i)}</p>
+    <p class="weekly-calendar-day">${i}</p>`
 
-  for(let i = date; i <= daysInMonth; i++){
-    const weeklyDayEl = document.createElement('div')
-    weeklyDayEl.addEventListener("click",() =>{
-      console.log(i)
+    document.querySelector('#weekly-calendar').appendChild(weeklyDayEl);
+    }else{
+      const weeklyDayEl = document.createElement('div')
+      weeklyDayEl.addEventListener("click",() =>{
+      console.log("clicked", mondayDate + i )
     })
     weeklyDayEl.className = "day-container"
-    weeklyDayEl.innerHTML = `<p>${getWeekDayName(i)}</p><p class="weekly-calendar-day">${i}</p>`
-    if(i === date){
+    weeklyDayEl.innerHTML = 
+    `<p>${getWeekDayName(currentYear,currentMonth,mondayDate + i)}</p>
+    <p class="weekly-calendar-day">${mondayDate + i}</p>`
+    
+    if(mondayDate + i === currentDate){
       const currentWeekDay = weeklyDayEl.querySelector('.weekly-calendar-day')
       currentWeekDay.classList.add('current-day')
     }
     document.querySelector('#weekly-calendar').appendChild(weeklyDayEl);
+    }
   }
+
+  // for(let i = date; i <= daysInMonth; i++){
+  //   const weeklyDayEl = document.createElement('div')
+  //   weeklyDayEl.addEventListener("click",() =>{
+  //     console.log(i)
+  //   })
+  //   weeklyDayEl.className = "day-container"
+  //   weeklyDayEl.innerHTML = `<p>${getWeekDayName(i)}</p><p class="weekly-calendar-day">${i}</p>`
+  //   if(i === date){
+  //     const currentWeekDay = weeklyDayEl.querySelector('.weekly-calendar-day')
+  //     currentWeekDay.classList.add('current-day')
+  //   }
+  //   document.querySelector('#weekly-calendar').appendChild(weeklyDayEl);
+  // }
   monthYearDay.innerHTML = `${dateObj.toLocaleDateString('cet',{
     weekday: 'short',
     day: 'numeric',
     month: 'short',
     year: '2-digit'
   })}`
-  monthYear.innerHTML = `${months[month]} ${year}`
-  console.log(date)
+  monthYear.innerHTML = `${months[currentMonth]} ${currentYear}`
+  
 }
 
 
